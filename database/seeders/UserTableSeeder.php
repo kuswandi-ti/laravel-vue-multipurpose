b7728d08927f28d5bad1f5f9e9e31c5fe16fe2ce
Kuswandi <kuswandi.ti@gmail.com>
2023-06-21 18:44:04 +0700

    15th Commit - Create Factory & Seeder User

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\Models\User;
+use Illuminate\Support\Str;
 use Illuminate\Database\Seeder;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 
@@ -15,11 +16,13 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        // User::create([
-        //     'name' => 'Kuswandi',
-        //     'email' => 'kuswandi.ti@gmail.com',
-        //     'password' => bcrypt('rahasia'),
-        // ]);
+        User::create([
+            'name' => 'Kuswandi',
+            'email' => 'kuswandi.ti@gmail.com',
+            'email_verified_at' => now(),
+            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token' => Str::random(10),
+        ]);
 
         User::factory(50)->create();
     }
